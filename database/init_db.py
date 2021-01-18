fd283241fb29e3a5e31f513fa9819283af7d85ee
hlyl <hlyl@novonordisk.com>
2021-01-18 07:06:00 +0100

    init via SQL file

--- database/init_db.py
+++ database/init_db.py
@@ -1,38 +1,5 @@
-"""
-from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String
+
+from sqlalchemy import engine
 
 
-class InitDatabase(object):
-    meta = MetaData()
-    items = Table(
-        "items",
-        meta,
-        Column("id", Integer, primary_key=True),
-        Column("name", String),
-        Column("nominal", Integer),
-        Column("min", Integer),
-        Column("restock", Integer),
-        Column("lifetime", Integer),
-        Column("usage", String),
-        Column("tier", String),
-        Column("rarity", String, default = "undefined"),
-        Column("cat_type", String),
-        Column("item_type", String),
-        Column("sub_type", String),
-        Column("mod", String, default="Vanilla"),
-        Column("trader", Integer),
-        Column("dynamic_event", Integer),
-        Column("count_in_cargo", Integer),
-        Column("count_in_hoarder", Integer),
-        Column("count_in_map", Integer),
-        Column("buyprice", Integer, nullable=True),
-        Column("count_in_player", Integer, nullable=True),
-        Column("sellprice", Integer, default=0),
-        Column("traderExclude", Integer, default=0),
-        Column("traderCat", String, nullable=True),
-        
-    )
-    def __init__(self, db_name):
-        engine = create_engine(f"sqlite:///{db_name}")
-        self.meta.create_all(engine)"""
-        
+
--- ui/db.py
+++ ui/db.py
@@ -65,14 +65,23 @@ class DB(object):
         if newDataBase:
             #InitDatabase(db_name)
             Item(db_name)
-
             self.config.set_database(db_name)
-            # self.manage_ini.write_ini(
-            #     section="Database",
-            #     sub_section="Database_Name",
-            #     value = db_name
-            # )
             self.db_status.set("Database connected to: " + db_name)
+            engine = create_engine(f"sqlite:///{db_name}")
+            Base.metadata.create_all(engine)
+            raw_connection = engine.raw_connection()
+            c = raw_connection.cursor()
+            c.execute("select count(*) from items")
+            nrows = c.fetchall()[0][0]
+
+            print(f"number of rows in database: { nrows }")
+            if nrows == 0:
+                print(f"Initializing rows using 'init.sql'")
+                raw_connection.cursor().executescript(open("../init.sql").read())
+                c.execute("select count(*) from items")
+                nrows = c.fetchall()[0][0]
+                print(f"Inserted { nrows } in the database")
+            raw_connection.commit()
         else:
             if Dao(db_name).items_table_exist():
 
