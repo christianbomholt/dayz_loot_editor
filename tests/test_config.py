cba7f066ca91b249b1a21ce15d2e7b9bd3347dc3
hlyl <hlyl@novonordisk.com>
2021-01-18 06:25:58 +0100

    test fix

--- tests/test_config.py
+++ tests/test_config.py
@@ -4,7 +4,7 @@ config = ConfigManager("config.xml")
 
 def test_config_getters():
   print(config.get_tiers())
-  assert config.get_traders() == ['Trader 1', 'Trader 2', 'Trader 3']
+  assert config.get_traders() == ['Trader 1', 'Trader 2', 'Trader 3',"EXCLUDE"]
   assert config.get_tiers() == ['Tier1', 'Tier2', 'Tier3', 'Tier4']
   #TODO: add test of config.get_tree_heading()
   
