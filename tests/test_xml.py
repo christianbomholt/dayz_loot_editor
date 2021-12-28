160c172bd9dbd685d635e72221cb394c9dbecd44
christianbomholt <chr.bomholt@gmail.com>
2021-12-28 16:19:51 +0100

    flake8 corrections

--- tests/test_xml.py
+++ tests/test_xml.py
@@ -1,22 +1,5 @@
-from xml_manager.xml_writer import XMLWriter
 from xml_manager.xml_parser import XMLReader
-from database import Dao
 from model import Item
-
-dbtest = Dao("test.db")
-
-
-# def test__export_xml():
-#     print("DEBUG  : I am in the test function and initiated xml_writer")
-#     database = Dao("test.db")
-#     file = "./tests/test.xml"
-#     selected_Mods = ("Vanilla", "Mod 1", "Mod 2")
-
-#     xml_writer = XMLWriter(filename=file)
-#     print("DEBUG  : I am in the test function and initiated xml_writer" + file)
-#     items = database.session.query(Item).filter(
-#         Item.mod.in_(selected_Mods))
-#     xml_writer.export_xml(items, "Normal map")
 
 
 def test__roundtrip():
