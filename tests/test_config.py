a92061dda054f958a7937369f2a7340390562a9f
Henrik Lynge <hlyl@novonordisk.com>
2021-12-28 22:05:59 +0100

    test_config

--- tests/test_config.py
+++ tests/test_config.py
@@ -2,10 +2,10 @@ from config import ConfigManager
 
 config = ConfigManager("config.xml")
 
-def test_config_getters():
-  print(config.get_tiers())
-  assert config.get_traders() == ['Trader 1', 'Trader 2', 'Trader 3',"EXCLUDE"]
-  #assert config.get_tiers() == ['Tier1', 'Tier2', 'Tier3', 'Tier4']
-  #TODO: add test of config.get_tree_heading()
-  
 
+def test_config_getters():
+    print(config.get_tiers())
+    assert config.get_traders() == ['Trader 1',
+                                    'Trader 2',
+                                    'Trader 3',
+                                    "EXCLUDE"]
