8ae8a49d8140d444fda88476d633fcc70759d58e
Henrik Lynge <hlyl@novonordisk.com>
2021-12-28 21:44:35 +0100

    fix spawnable

--- utility/spawnable.py
+++ utility/spawnable.py
@@ -1,32 +1,38 @@
-from tkinter import Tk, filedialog,OptionMenu
-from model.item import Base, Item, LinkBulletMag, LinkBullets, LinkAttachments, LinkMags, Attachments, Magazines, Bullets
+from tkinter import filedialog
+from model.item import Base, Item, LinkBullets, LinkAttachments, LinkMags, Attachments, Magazines, Bullets
 import xml.etree.cElementTree as ET
 
 
 def exportSpawnable(session, items):
-    fname  = filedialog.asksaveasfilename(filetypes=[("xml file", ".xml")],defaultextension=".xml")
+    fname = filedialog.asksaveasfilename(
+        filetypes=[("xml file", ".xml")], defaultextension=".xml")
     if fname is not None:
-        weapons = items.filter(Item.item_type=="ranged")
+        weapons = items.filter(Item.item_type == "ranged")
         root = ET.Element("type")
         for weapon in weapons:
-        
             if weapon.nominal > 0:
-                name=weapon.name        
-                tagtype = ET.SubElement(root,"type", name = str(name))
-                tagattach = get_class(session,"attachments",weapon.name)
-                tagmags = get_class(session,"magazines",weapon.name)
+                name = weapon.name
+                tagtype = ET.SubElement(root, "type", name=str(name))
+                tagattach = get_class(session, "attachments", weapon.name)
+                tagmags = get_class(session, "magazines", weapon.name)
 
                 if tagattach is not None:
-                    write_subelement(tagtype,"attachments", "hndgrd", 0.75, tagattach)
-                    write_subelement(tagtype,"attachments", "bttstck", 0.75, tagattach)
-                    write_subelement(tagtype,"attachments", "optic", 0.25, tagattach)
-                    write_subelement(tagtype,"attachments", "comp", 0.25, tagattach)
-                    write_subelement(tagtype,"attachments", "suppressor", 0.25, tagattach)
-                    write_subelement(tagtype,"attachments", "light", 0.25, tagattach)
-                    write_remainder(tagtype,"attachments",0.50,tagattach)
-                if tagmags is not None :    
-                    write_subelement(tagtype,"mags", "mag", 0.75, tagmags)
-        
+                    write_subelement(tagtype, "attachments",
+                                     "hndgrd", 0.75, tagattach)
+                    write_subelement(tagtype, "attachments",
+                                     "bttstck", 0.75, tagattach)
+                    write_subelement(tagtype, "attachments",
+                                     "optic", 0.25, tagattach)
+                    write_subelement(tagtype, "attachments",
+                                     "comp", 0.25, tagattach)
+                    write_subelement(tagtype, "attachments",
+                                     "suppressor", 0.25, tagattach)
+                    write_subelement(tagtype, "attachments",
+                                     "light", 0.25, tagattach)
+                    write_remainder(tagtype, "attachments", 0.50, tagattach)
+                if tagmags is not None:
+                    write_subelement(tagtype, "mags", "mag", 0.75, tagmags)
+
         prettify(root)
         tree = ET.ElementTree(root)
         tree.write(fname)
@@ -39,12 +45,15 @@ def write_subelement(root, tag, grp, prop, item_list):
         if grp in item.name.lower():
             check = True
             break
-    if check is True:        
-        attach_sub = ET.SubElement(root, tag, chance=str(chance))  
+    if check is True:
+        attach_sub = ET.SubElement(root, tag, chance=str(chance))
         for item in item_list:
             if grp in item.name.lower():
-                ET.SubElement(attach_sub, "item", name=item.name, chance=str(item.prop/100)) 
+                ET.SubElement(attach_sub, "item", name=item.name,
+                              chance=str(item.prop/100))
                 item_list.remove(item)
+
+
 """
 def write_subelement(root, tag, grp, prop, item_list):
     chance = prop
@@ -61,41 +70,45 @@ def write_subelement(root, tag, grp, prop, item_list):
                 print("DEBUG write_subelement  :", item)
                 #item_list.remove(item)"""
 
+
 def write_remainder(root, tag, prop, item_list):
     chance = prop
-    attach_sub = ET.SubElement(root, tag, chance=str(chance))  
+    attach_sub = ET.SubElement(root, tag, chance=str(chance))
     for item in item_list:
-        ET.SubElement(attach_sub, "item", name=item.name, chance=str(item.prop/100))
+        ET.SubElement(attach_sub, "item", name=item.name,
+                      chance=str(item.prop/100))
 
 
 def get_class_by_tablename(tablename):
     for c in Base._decl_class_registry.values():
         if hasattr(c, '__tablename__') and c.__tablename__ == tablename:
-            return c 
+            return c
+
 
 def get_class(session, tablename, name):
     class_ = get_class_by_tablename(tablename)
     result = session.query(
-            class_
-        ).filter(
-             Item.name == name,
-        ).filter(
-            Item.name == LinkMags.itemname
-        ).filter(
-            LinkMags.magname == Magazines.name
-        ).filter(
-            Item.name == LinkBullets.itemname
-        ).filter(
-            LinkBullets.bulletname == Bullets.name
-        ).filter(
-            Item.name == LinkAttachments.itemname
-        ).filter(
-            LinkAttachments.attachname == Attachments.name
-        ).filter(
-            class_.prop>0
-        ).all()
+        class_
+    ).filter(
+        Item.name == name,
+    ).filter(
+        Item.name == LinkMags.itemname
+    ).filter(
+        LinkMags.magname == Magazines.name
+    ).filter(
+        Item.name == LinkBullets.itemname
+    ).filter(
+        LinkBullets.bulletname == Bullets.name
+    ).filter(
+        Item.name == LinkAttachments.itemname
+    ).filter(
+        LinkAttachments.attachname == Attachments.name
+    ).filter(
+        class_.prop > 0
+    ).all()
     if len(result) > 0:
         return result
+
 
 def prettify(element, indent='  '):
     queue = [(0, element)]  # (level, element)
@@ -108,4 +121,4 @@ def prettify(element, indent='  '):
             element.tail = '\n' + indent * queue[0][0]  # for sibling open
         else:
             element.tail = '\n' + indent * (level-1)  # for parent close
-        queue[0:0] = children  # prepend so children come before siblings        
+        queue[0:0] = children  # prepend so children come before siblings
