0d3d1c82b5faedac0be19d0f908f4b9a3325f382
hlyl <hlyl@novonordisk.com>
2023-09-11 22:41:46 +0200

    fix test

--- tests/test_config.py
+++ tests/test_config.py
@@ -2,10 +2,9 @@ from config import ConfigManager
 
 config = ConfigManager("config.xml")
 
+expected = "Trader 1"
+
 
 def test_config_getters():
     print(config.get_tiers())
-    assert config.get_traders() == ['Trader 1',
-                                    'Trader 2',
-                                    'Trader 3',
-                                    "EXCLUDE"]
+    assert expected in config.get_traders()
